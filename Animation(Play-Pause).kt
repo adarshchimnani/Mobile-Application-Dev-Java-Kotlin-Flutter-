1da112d556290a5976c64309402803cfeeab1c33
adarshchimnani <67822194+adarshchimnani@users.noreply.github.com>
2022-11-24 16:05:30 +0500

    Animation Basics (Play-Pause-Resume) using Ui Thread

---
+++ Animation(Play-Pause).kt
@@ -0,0 +1,30 @@
+package com.example.lab6
+
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.Toast
+import android.widget.ToggleButton
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+  
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+}

0f659f0215cde35d22ed7387a9e83714686c3ecc
adarshchimnani <67822194+adarshchimnani@users.noreply.github.com>
2022-11-24 16:12:02 +0500

    Initialized button and image variables

--- Animation(Play-Pause).kt
+++ Animation(Play-Pause).kt
@@ -15,16 +15,20 @@ import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-  
+    lateinit var image: ImageView
+    lateinit var togBtn: ToggleButton
+    lateinit var anim: ObjectAnimator
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        image = findViewById(R.id.image)
+        togBtn = findViewById(R.id.toggleBtn)
 
-
+     
     }
 
-
+   
 }
